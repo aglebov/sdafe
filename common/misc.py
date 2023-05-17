2ea4b75bdb11d2ac1066bb90b98fe01f3a6a8ae9
Artem Glebov <artem.glebov@gmail.com>
2023-05-17 17:29:25 +0100

    Move reduce_model to sdafe-utils

--- common/misc.py
+++ common/misc.py
@@ -11,31 +11,3 @@ def aic(fit):
 
 def bic(fit, n):
     return 2 * fit.fun + np.log(n) * len(fit.x)
-
-
-def reduce_model(x, y, fit_f):
-    """A simplistic version of the stepAIC function from R's MASS package"""
-    while len(x.columns) > 1:
-        baseline_fit = fit_f(sm.add_constant(x), y)
-        print(f'Variables: {list(x.columns)}')
-        
-        res = []
-        res.append(baseline_fit.aic)
-
-        for col in x.columns:
-            reduced_x = x.drop(col, axis=1)
-            fit = fit_f(sm.add_constant(reduced_x), y)
-            res.append(fit.aic)
-
-        res = pd.DataFrame(res, columns=['AIC'], index=['<none>'] + list(x.columns))
-        res.sort_values(by='AIC', inplace=True)
-        print(res)
-        
-        if res.iloc[0, 0] < baseline_fit.aic:
-            print(f'Dropping variable: {res.index[0]}\n')
-            x = x.drop(res.index[0], axis=1)
-        else:
-            print('Stopping')
-            break
-    print('One variable left in the model - stopping')
-    return list(x.columns)
